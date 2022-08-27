baa957496bd824d3b5ce7634fecfc30cd7267035
Alex Cornellier <alex@cornellier.com>
2022-08-27 11:42:48 -0400

    remove debug shortcuts

--- Assets/Scripts/Utils/DebugShortcuts.cs
+++ Assets/Scripts/Utils/DebugShortcuts.cs
@@ -2,21 +2,11 @@
 using UnityEngine.SceneManagement;
 using Zenject;
 
-// TODO: DELETE THIS BEFORE PUBLISHING
 public class DebugShortcuts : MonoBehaviour
 {
-    [Inject] SavedStateManager _savedStateManager;
-
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.R))
-        {
-            _savedStateManager.Reset();
+        if (Input.GetKeyDown(KeyCode.R))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-        else if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
     }
 }
