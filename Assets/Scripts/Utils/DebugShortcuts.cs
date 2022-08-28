4c7649d459bfb205100e41aff9845eb541868c38
Alex Cornellier <alex@cornellier.com>
2022-08-27 22:32:21 -0400

    windows full screen toggle

--- Assets/Scripts/Utils/DebugShortcuts.cs
+++ Assets/Scripts/Utils/DebugShortcuts.cs
@@ -7,6 +7,15 @@ public class DebugShortcuts : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
+        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (Screen.fullScreen)
+                Screen.SetResolution(1920, 1080, false);
+            else
+                Screen.fullScreen = true;
+        }
     }
 }
